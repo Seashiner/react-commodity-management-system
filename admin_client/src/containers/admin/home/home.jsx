332af69a1450d26f0a22d5892e276b0fed21227b
Seashiner <2729132783@qq.com>
2020-05-05 23:49:19 +0800

    二级导航

--- admin_client/src/containers/admin/home/home.jsx
+++ admin_client/src/containers/admin/home/home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import sun from './images/animat-day-night-color.gif'
+import './css/home.less'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        home
+      <div className = 'home'>
+        <img src={sun}/>
+        <h1>商品管理系统</h1>
       </div>
     )
   }
